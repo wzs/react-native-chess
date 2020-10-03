672ec00da48b5cf2dd439da775fa9b33decbc860
Pawel Wrzosek <pawelwrzosek@gmail.com>
2020-10-04 01:11:06 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import {AppRegistry, View} from 'react-native';
+import {name as appName} from './app.json';
+import React from 'react';
+
+const App = () => <View />;
+
+AppRegistry.registerComponent(appName, () => App);
