7fa5c4eeb4039d33479db7f61851bd9c37d70e2d
Pawel Wrzosek <pawelwrzosek@gmail.com>
2020-10-04 17:26:17 +0200

    feat: add react-native-web representation

---
+++ docs/precache-manifest.fab38c36a5616a5f10684b92c5de909c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c97c07e9edb7d800d37b5fb63dedebd",
+    "url": "/react-native-chess/index.html"
+  },
+  {
+    "revision": "99cd033a46f587e5924b",
+    "url": "/react-native-chess/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9b47478754a05482daf7",
+    "url": "/react-native-chess/static/js/2.27beddcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-native-chess/static/js/2.27beddcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99cd033a46f587e5924b",
+    "url": "/react-native-chess/static/js/main.2d174269.chunk.js"
+  },
+  {
+    "revision": "5c0f17ca71be68e610d5",
+    "url": "/react-native-chess/static/js/runtime-main.62f23eb1.js"
+  }
+]);
---
+++ docs/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-native-chess/precache-manifest.fab38c36a5616a5f10684b92c5de909c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-native-chess/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
